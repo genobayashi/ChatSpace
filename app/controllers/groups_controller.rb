f55c46aa224017e1c9489fb7b1e9d94a5322c284
genobayashi <juventus1025@gmail.com>
2017-03-16 14:10:37 +0900

    Edit_group_controller_new_&_create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,20 @@
 class GroupsController < ApplicationController
 
   def new
-    
+    @group = Group.new
   end
 
   def edit
     
+  end
+
+  def create
+    Group.create(group_params)
+  end
+
+  private
+  def group_params
+    params.permit(:name)
   end
 
 end
