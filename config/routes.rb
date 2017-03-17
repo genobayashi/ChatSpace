69debe9cad5dc51bb8027b5f7cb8c0e27d5f9e77
genobayashi <juventus1025@gmail.com>
2017-03-17 14:12:43 +0900

    Edit_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'chats#index'
-  resources :groups, except: [:index, :show, :destroy]
+  resources :groups, except: [:index, :show, :destroy] do
+    resources :message, only: [:index]
+  end
 end
