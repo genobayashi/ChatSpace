50865884dc1230c578152febadaf4eed223ff3ee
genobayashi <juventus1025@gmail.com>
2017-03-17 16:32:05 +0900

    Correction_routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'chats#index'
   resources :groups, except: [:index, :show, :destroy] do
-    resources :message, only: [:index]
+    resources :messages, only: [:index]
   end
 end
