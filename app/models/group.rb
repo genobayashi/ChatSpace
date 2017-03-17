90eb53429943acbb2fbd2f826e3b50a3ef3c0bde
genobayashi <juventus1025@gmail.com>
2017-03-17 22:00:57 +0900

    accepts_nested_attributes_for_user_groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :user_groups
   has_many :users, through: :user_groups
+  accepts_nested_attributes_for :user_groups
 
   validates :name, presence: true
 end
