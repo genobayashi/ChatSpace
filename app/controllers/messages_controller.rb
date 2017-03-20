eba504b4cd9610d95c83b7b7d0ba5aa225bdd0cc
genobayashi <juventus1025@gmail.com>
2017-03-20 21:25:46 +0900

    Edit_messages_controller_create_&_flash
    メッセージをDBに保存できるようにし、失敗時にフラッシュメッセージをだす

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,30 @@
 class MessagesController < ApplicationController
+
+  before_action :group_instance
+
   def index
+
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group)
+    else
+      flash.now[:alert] = 'メッセージの投稿に失敗しました。'
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params[:user_id] = current_user.id
+    params.permit(:body, :image, :group_id, :user_id)
+  end
+
+  def group_instance
     @group = Group.find(params[:group_id])
     @groups = current_user.groups
   end
+
 end
