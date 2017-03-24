04b5b00a9d19ff7a2327600e8aa8de8be9bf7ec3
genobayashi <juventus1025@gmail.com>
2017-03-24 14:08:08 +0900

    Define_factorygirl

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,10 +1,14 @@
 FactoryGirl.define do
 
   factory :message do
-    body     "juventus"
-    image    "test.png"
-    user_id  1
-    group_id 1
-  end
+    body       {Faker::Lorem.word}
+    image      "test.png"
+    user_id    1
+    group_id   1
+    created_at {Faker::Time.between(2.days.ago, Time.now)}
 
+    factory :invalid_message do
+      body ""
+    end
+  end
 end
