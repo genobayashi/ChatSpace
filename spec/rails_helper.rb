fe49fd9dc39ef211ce506f7fa8d8d96830c1c10a
genobayashi <juventus1025@gmail.com>
2017-03-24 14:07:12 +0900

    Edit_rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'devise'
-require File.expand_path("spec/support/controller_macros.rb")
+require 'support/controller_macros'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -58,7 +58,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
-  config.include ControllerMacros, type: :controller
+  config.extend ControllerMacros, type: :controller
 
   [:controller, :view, :request].each do |type|
     config.include ::Rails::Controller::Testing::TestProcess, :type => type
