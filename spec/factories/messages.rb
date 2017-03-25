cfcb2dd5726f4ab2a0cb6517b4c39e6f008e5221
genobayashi <juventus1025@gmail.com>
2017-03-25 14:11:17 +0900

    Dfine_image_culumn

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :message do
     body       {Faker::Lorem.word}
-    image      "test.png"
+    image      {Faker::File.file_name('path/to')}
     created_at {Faker::Time.between(2.days.ago, Time.now)}
 
     trait :with_user_and_message do
