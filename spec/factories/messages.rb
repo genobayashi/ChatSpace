5b3cb6a0f75cfa13b5c1720c884f0d9d3cfb71e9
genobayashi <juventus1025@gmail.com>
2017-03-25 13:04:22 +0900

    Factory_Girl_assosiation
    message,group,userを紐づける

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,9 +3,12 @@ FactoryGirl.define do
   factory :message do
     body       {Faker::Lorem.word}
     image      "test.png"
-    user_id    {Faker::Number.between(1,100)}
-    group_id   {Faker::Number.between(1,100)}
     created_at {Faker::Time.between(2.days.ago, Time.now)}
+
+    trait :with_user_and_message do
+      user
+      group
+    end
 
     factory :invalid_message do
       body ""
