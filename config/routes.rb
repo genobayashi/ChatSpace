913281c795494c3828e9ccc1a32b02e679e2b5dc
genobayashi <juventus1025@gmail.com>
2017-03-29 18:22:11 +0900

    Edit_routes_for_create_ajax_user_list_action
    グループコントローラーにアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :groups, except: [:show, :destroy] do
+    collection do
+      get  :ajax_user_list
+    end
     resources :messages, only: [:index, :create]
   end
 end
