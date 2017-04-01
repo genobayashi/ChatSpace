a876d7bb09e4a0a9e6a0aaff915a16959ba564b5
genobayashi <juventus1025@gmail.com>
2017-04-01 15:59:02 +0900

    Edit_group_js

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,44 +1,64 @@
-$(function() {
-  var list = $("#user-list");
-  var preWord;
+document.addEventListener("turbolinks:load", function() {
+  $(function() {
+    var list = $("#user-list");
+    var no_member = $('<p class="no-member"> 一致するメンバーはいません </p>')
+    var preWord;
 
-  function appendList(word) {
-    var item = $('<li class="list">').append(word);
-    list.append(item);
-  }
+    function appendList(user) {
+      var item = $(`<li class="list" data-user_nickname=${user.nickname} data-user_id=${user.id}>
+                    ${user.nickname}
+                    <a class="add-user"> 追加 </a>
+                    </li>`);
+      list.append(item);
+    }
 
-  function editElement(element) {
-    var result = "%" + element + "%";
-    return result;
-  }
+    function add_user_result(nickname, user_id) {
+      var member = $(`<li class="chat-group-user">
+                        <input name="group[user_ids][]" type="hidden" value=${user_id}>
+                        ${nickname}
+                        <a class="remove-user"> 削除 </a>
+                      </li>`);
+      $('.field-input').append(member);
+    }
 
-  $("#keyword").on("keyup", function() {
-    var input = $("#keyword").val();
-    var inputs = input.split(" ").filter(function(e) { return e; });
-    var newInputs = inputs.map(editElement);
-    var word = newInputs.join("|");
-    $.ajax({
-      type: 'GET',
-      url: '/groups/ajax_user_list',
-      data: {
-        user_nickname: word
-      },
-      dataType: 'json'
-    })
-    .done(function(data) {
-      var users_info = (data.user_list.user);
-      $(".list").remove();
-      if(word != preWord && input.length !== 0){
-        $.each(users_info, function(i, user){
-          appendList(user.nickname);
-        });
-        if($(".list").length === 0){
-          appendList("一致するメンバーはいません");
+    $("#keyword").on("keyup", function() {
+      var input = $("#keyword").val();
+      $.ajax({
+        type: 'GET',
+        url: '/groups/ajax_user_list',
+        data: {
+          user_nickname: input
+        },
+        dataType: 'json'
+      })
+      .done(function(data) {
+        var users_info = data.user_list.user;
+        if ( input != preWord ) {
+          $('.list').remove();
+          if(input.length !== 0) {
+            $.each(users_info, function(i, user){
+              appendList(user);
+            });
+            if($(".list").length === 0){
+              list.append(no_member);
+            }
+          }
         }
-      }
-    })
-    .fail(function(){
-      alert('error');
+      })
+      .fail(function(){
+        alert('error');
+      });
+    });
+
+    $("#user-list").on('click','.add-user', function(){
+      $(this).parent().remove();
+      var nickname = $(this).parent().data('user_nickname');
+      var user_id = $(this).parent().data('user_id');
+      add_user_result(nickname, user_id);
+    });
+
+    $(".field-input").on('click', '.remove-user', function(){
+      $(this).parent().remove();
     });
   });
-});
+})
