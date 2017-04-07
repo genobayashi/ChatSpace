07000535ef3fcdf163c4a64b4bdafd7b7b4d8624
genobayashi <juventus1025@gmail.com>
2017-04-07 14:15:34 +0900

    Edit_messages#index_for_json

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,13 +7,17 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
+    respond_to do |format|
+      format.html
+      format.json { render "update_message.json.jbuilder" }
+    end
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-        format.html { redirect_to group_messages_path(@group) }
+        format.html { redirect_to group_messages_path(group) }
         format.json { render "message_index.json.jbuilder"}
       end
     else
